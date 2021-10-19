8cb813d34a9edbeef8abd0610c4b596df2cd9206
Robert Valenzuela <robert_valenzuela@usmp.pe>
2021-10-19 17:17:05 -0500

    first commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjercicioSem11_Teoria.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        
+        //public DbSet<SolicitudAdopcion> SolicitudesAdopcion { get; set; }
+        //public DbSet<Usuario> Usuarios { get; set; }
+
+        //public DbSet<PruebaMVCLogin.Models.Contact> DataContacts { get; set; }
+        //public DbSet<PruebaMVCLogin.Models.Product> DataProducts { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EjercicioSem11_Teoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
